f4c21b28542fdacbb0f7b81862650504dfe427f5
tlobnow <70801489+tlobnow@users.noreply.github.com>
2021-02-27 14:38:16 +0100

    Add files via upload

---
+++ Primer Spreadsheet Filter.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(dplyr)
+
+
+Spreadsheet <- read.csv("https://raw.githubusercontent.com/tlobnow/Cryptosporidium-BSc/master/Primer%20Spreadsheet%20-%20Primer%20Sheet%20no%20merged%20cells.csv")
+Spreadsheet
+
+Spreadie <- as.data.frame(Spreadsheet)
+Spreadie
+
+
+subset.data.frame()
+
