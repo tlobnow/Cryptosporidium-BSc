3a057f5d7df30c4455f58b0129b263c1b649cb31
Finn_PC <tlobnow@googlemail.com>
2021-11-09 07:22:22 +0100

    - added ML trees - TODO: NJ trees

--- Analysis/LGC_Seq/On_PC.R
+++ Analysis/LGC_Seq/On_PC.R
@@ -1,13 +1,11 @@
 #install CRAN Task View for phylogenetics install.packages('ctv')
-library('ctv') 
 #install.views('Phylogenetics')
 #update.views('Phylogenetics')
 
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("ggtree")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("ggtree")
 
 library(ctv)
 library(ape)
@@ -15,47 +13,138 @@ library(ggtree)
 library(phytools)
 library(ggtree)
 library(tidyverse)
-
+library(tibble)
 
 
 ###############################################################################
 ###############################################################################
 # 1. COWP
 
-tree <- read.nexus('~/Trees 2.0/COWP-Tree.nex')
+
+tree <- read.tree("~/Cryptosporidium-BSc/Analysis/Trees 2.0/COWP-Tree.phy_phyml_tree.txt")
 
 
-p <- ggtree(tree, layout = "rectangular") + 
+bs_tibble <- tibble(
+  node=1:Nnode(tree) + Ntip(tree),
+  bootstrap = ifelse(tree$node.label < 0.01, "", tree$node.label))
+
+ggtree(tree) %<+% bs_tibble +
+  geom_text(aes(label=bootstrap), hjust=2, nudge_y = 1, size = 3) +
+  geom_tiplab(aes(label=label))
+
+
+p <-ggtree(tree) %<+% bs_tibble +
   geom_tiplab() +
   xlim(NA, 0.1) +
-  geom_treescale()
+  geom_treescale() +
+  geom_text(aes(label=bootstrap), hjust=1.3, nudge_y = 0.5, size = 3)
+
+
+p %>% flip(55,56)  +
+  geom_strip("NZ_1642", "AA_0585", barsize=2, color='blue', 
+             label="C 2", offset.text=.001, offset = 0.01, extend = 1) +
+  geom_strip('CR_2084', 'CR_2128', barsize=2, color='red', 
+             label="C 1", offset.text=.001, offset = 0.01, extend = 0.5) +
+  geom_strip('Par-COWP', 'Mel-COWP', barsize=2, color='darkgreen', 
+             label=
+               "C.parvum
+C.hominis
+C.meleagridis", offset.text=.001, offset = .02, fontsize = 3) +
+  geom_strip('Tyz-COWP', 'CR_2128', barsize=2, color='purple', 
+             label="C.tyzzeri", offset.text=.001, offset = .02, extend = 0.5) +
+  ggtitle("COWP Neighbor-Joining Tree", subtitle = "Genetic Distance Tamura-Nei, C.parvum Outgroup") +
+  theme(plot.title = element_text(hjust = 0.5, size = 30),
+        plot.subtitle = element_text(hjust = 0.5, size = 20))
+  
+
+
+
+
+###############################################################################
+###############################################################################
+# 3. GP60
+
+
+tree <- read.tree("~/Cryptosporidium-BSc/Analysis/Trees 2.0/GP60-Tree.phy_phyml_tree.txt")
+
+bs_tibble <- tibble(
+  node=1:Nnode(tree) + Ntip(tree),
+  bootstrap = ifelse(tree$node.label < 0.9, "", tree$node.label))
+
+ggtree(tree)%<+% bs_tibble +
+  geom_text(aes(label=bootstrap), hjust=1, nudge_y = 0.5, size = 3) +
+  geom_tiplab(aes(label=label))
+
+
+p <-ggtree(tree) %<+% bs_tibble +
+  geom_tiplab() +
+  xlim(NA,0.59) +
+  geom_treescale()  +
+  geom_text(aes(label=bootstrap), hjust=1.3, nudge_y = 0.5, size = 3)
+
+p %>% flip(81,84) +
+  #geom_text(aes(label=node), nudge_x = -0.002) +
+  geom_strip('AA_0523', 'Tyz-GP60', barsize=2, color='blue', 
+             label="IXb", offset.text=.01, offset = 0.07, extend = 0.5) +
+  geom_strip('CR_2085', 'CR_2206', barsize=2, color='red', 
+             label="IXa", offset.text=.01, offset = 0.07) +
+  geom_strip('AA_0523', 'CR_2206', barsize=2, color='purple', 
+             label="C.tyzzeri", offset.text=.01, offset = .1, extend = 0.6) +
+  geom_strip('AA_0523', 'G_2136', barsize = 2, color = "blue",
+             label = 'IXb.1', offset.text = .006, offset = 0.03, extend = 0.5) +
+  geom_strip('CR_2163', 'G_3224', barsize = 2, color = "blue",
+             label = 'IXb.2', offset.text = .006, offset = 0.03, extend = 0.4) +
+  geom_strip('AA_0144', 'NZ_1644', barsize = 2, color = "blue",
+             label = 'IXb.3', offset.text = .006, offset = 0.03, extend = 0.8) +
+  geom_strip('Hom-GP60', 'Par-GP60', barsize=2, color='darkgreen', 
+             label=
+"C.meleagridis
+C.hominis
+C.parvum", offset.text=.01, offset = .1, extend = 1, fontsize = 2.5) +
+  ggtitle("GP60 Neighbor-Joining Tree", subtitle = "Genetic Distance Tamura-Nei, C.parvum Outgroup") +
+  theme(plot.title = element_text(hjust = 0.5, size = 30),
+        plot.subtitle = element_text(hjust = 0.5, size = 20))
+  
+
+
+
+
+###############################################################################
+###############################################################################
+# 4. MEDLE
+
+
+tree <- read.tree("~/Cryptosporidium-BSc/Analysis/Trees 2.0/MEDLE-Tree.phy_phyml_tree.txt")
+
+bs_tibble <- tibble(
+  node=1:Nnode(tree) + Ntip(tree),
+  bootstrap = ifelse(tree$node.label < 0.9, "", tree$node.label))
+
+ggtree(tree)%<+% bs_tibble +
+  geom_text(aes(label=bootstrap), hjust=1, nudge_y = 0.5, size = 3) +
+  geom_tiplab(aes(label=label))
+
+
+p <-ggtree(tree) %<+% bs_tibble +
+  geom_tiplab() +
+  xlim(NA,0.8) +
+  geom_treescale()  +
+  geom_text(aes(label=bootstrap), hjust=1.3, nudge_y = 0.5, size = 3)
 
 p +
-  geom_hilight(node=74
-               , fill="lightgreen", alpha=.6, type = "roundrect") +
-  geom_strip(29, 51, barsize=2, color='red', 
-             label="C 1", offset.text=.001, offset = -.03, extend = 0.85) +
-  geom_strip('G_2136', 'NZ_1642', barsize=2, color='blue', 
-             label="C 2", offset.text=.001, offset = -.03) +
-  geom_strip('Mel-COWP', 'Par-COWP', barsize=2, color='darkgreen', 
+  #geom_text(aes(label=node), nudge_x = -0.002) +
+  geom_strip('Tyz-MEDLE', 'AA_0209', barsize=2, color='blue', 
+             label="IXb", offset.text=.01, offset = -0.07, extend = 0.1) +
+  geom_strip('Tyz-MEDLE', 'AA_0209', barsize=2, color='purple', 
+             label="C.tyzzeri", offset.text=.01, offset = .1, extend = 0.1) +
+  geom_strip('Hom-MEDLE', 'Par-MEDLE', barsize=2, color='darkgreen', 
              label=
-  "C.meleagridis
-C.hominis
-C.parvum", offset.text=.001, offset = .01, extend = 1) +
-  geom_strip('AA_0559', 'NZ_1642', barsize=2, color='purple', 
-             label="C.tyzzeri", offset.text=.001, offset = .01) +
-  ggtitle("COWP NJ Tree")
-
-p
-
-### PHY-ML
-tree <- read.tree("~/PhyML-3.1/COWP-Tree.phy_phyml_tree.txt")
-p <- ggtree(tree) +
-  #geom_text(aes(label=node)) +
-  geom_tiplab() +
-  xlim(NA, 0.1) +
-  geom_treescale()
-p
+               "C.hominis
+C.meleagridis
+C.parvum", offset.text=.01, offset = .1, extend = 0.2, fontsize = 4) +
+  ggtitle("MEDLE Neighbor-Joining Tree", subtitle = "Genetic Distance Tamura-Nei, C.parvum Outgroup") +
+  theme(plot.title = element_text(hjust = 0.5, size = 30),
+        plot.subtitle = element_text(hjust = 0.5, size = 20))
 
 
 ###############################################################################
@@ -88,67 +177,8 @@ C.parvum", offset.text=.001, offset = .01, extend = 1) +
 
 
 
-###############################################################################
-###############################################################################
-# 3. GP60
 
 
-tree <- read.nexus('~/Trees 2.0/GP60-Tree.nex')
-
-
-p <- ggtree(tree) +
-  #geom_text(aes(label=node)) +
-  geom_tiplab() +
-  xlim(NA, 1) +
-  geom_treescale()
-p
-
-p %>% flip(67,69) %>% flip(44,66) %>% flip(64,65) +
-  geom_hilight(node=54, fill="lightgreen", alpha=.6, type = "roundrect") +
-  geom_strip('NZ_1640', 'AA_0571', barsize=2, color='red', 
-             label="T 1", offset.text=.001, offset = -0.055, extend = 0.2) +
-  geom_strip('CR_2084', 'AA_0578', barsize=2, color='blue', 
-             label="T 2", offset.text=.001, offset = -0.055) +
-  geom_strip('Hom-TRAP-C1', 'Par-TRAP-C1', barsize=2, color='darkgreen', 
-             label=
-               "C.hominis
-C.meleagridis
-C.parvum", offset.text=.001, offset = .01, extend = 1) +
-  geom_strip('AA_0559', 'NZ_1642', barsize=2, color='purple', 
-             label="C.tyzzeri", offset.text=.001, offset = .01)
-
-
-
-### PHY-ML
-tree <- read.tree("~/PhyML-3.1/GP60-Tree.phy_phyml_tree.txt")
-p <- ggtree(tree) +
-  geom_text(aes(label=node)) +
-  geom_tiplab() +
-  xlim(NA, 1) +
-  geom_treescale()
-p
-
-
-###############################################################################
-###############################################################################
-# 4. MEDLE
-
-
-tree <- read.nexus('~/Trees 2.0/MEDLE-Tree.nex')
-
-p <- ggtree(tree) + 
-  geom_tiplab()
-p
-
-
-### PHY-ML
-tree <- read.tree("~/PhyML-3.1/MEDLE-Tree.phy_phyml_tree.txt")
-p <- ggtree(tree) +
-  #geom_text(aes(label=node)) +
-  geom_tiplab() +
-  xlim(NA, 0.3) +
-  geom_treescale()
-p
 
 ###############################################################################
 ###############################################################################
--- Analysis/Wiki_DUKE.R
+++ Analysis/Wiki_DUKE.R
@@ -1,8 +1,109 @@
 library(phangorn)
 library(ape)
 
+##### MEDLE
 
-GP60  = read.phyDat("~/Documents/Programming/R/HZ_SC_and_Raw_Data/Cryptosporidium-BSc/Analysis/Trees 2.0/GP60-Tree.phy")
+MEDLE  = read.phyDat("~/Cryptosporidium-BSc/Analysis/Trees 2.0/MEDLE-Tree.phy")
+dm    = dist.dna(as.DNAbin(MEDLE))
+
+treeUPGMA = upgma(dm)
+treeNJ    = NJ(dm)
+
+layout(matrix(c(1,2)), height=c(2,2.5))
+
+plot(treeUPGMA, main = "UPGMA", cex = 1)
+plot(treeNJ, "unrooted", main = "NJ", cex = 1)
+
+parsimony(treeUPGMA, MEDLE)  # 317 <-- most parsimonious tree, has lowest score
+parsimony(treeNJ, MEDLE)     # 322
+
+optParsUPGMA = optim.parsimony(treeUPGMA, MEDLE)
+optParsNJ = optim.parsimony(treeNJ, MEDLE)
+
+plot(optParsUPGMA, main="UPGMA", cex = 1) # rooted tree on top
+plot(optParsNJ, "unrooted", main="NJ", cex = 1) # unrooted tree on bottom
+
+
+fit_treeUPGMA = pml(unroot(treeUPGMA), data = MEDLE)
+
+fit_treeUPGMA_opt1 = optim.pml(fit_treeUPGMA)
+layout(matrix(c(1,2)), height=c(1,1))
+par(mar = c(.1,.1,.1,.1))
+plot(fit_treeUPGMA, main="default branches", cex = 0.8)   # top = default branch lengths
+plot(fit_treeUPGMA_opt1, main="optimized branches", cex = 0.8)   # bottom = optimized branch lengths
+
+# In the graphics window, the tree on top shows the default branches, while the tree on the bottom shows the branches after optimization. 
+# Longer branches indicate that more molecular change has occurred along a given branch. 
+# You will find that the likelihood of the data under optimized branch lengths has increased considerably - from 1573.4 to 1555. 
+# We can compare these models using the 'AIC' function and we find unsurprisingly that the AIC is substantially lower for the tree with branches optimized.
+
+AIC(fit_treeUPGMA, fit_treeUPGMA_opt1)
+
+#We can also search for a better tree by re-arranging the branches, i.e., by setting the parameter 'optNni' to 'TRUE', which causes the function 'optim.pml' to optimize tree topology in addition to branch lengths:
+  
+  
+fit_treeUPGMA_opt2 = optim.pml(fit_treeUPGMA, optNni=TRUE)
+
+layout(matrix(c(1,2)), height=c(1,1))
+
+plot(fit_treeUPGMA_opt1, cex = 0.8)  # top = original topology with optimized branch lengths
+plot(fit_treeUPGMA_opt2, cex = 0.8)    # bottom = optimized topology AND branch lengths
+
+AIC(fit_treeUPGMA_opt1, fit_treeUPGMA_opt2)
+
+
+##### GST
+GST  = read.phyDat("~/Cryptosporidium-BSc/Analysis/Trees 2.0/GST-Tree.phy")
+dm    = dist.dna(as.DNAbin(GST))
+
+treeUPGMA = upgma(dm)
+treeNJ    = NJ(dm)
+
+layout(matrix(c(1,2)), height=c(2,2.5))
+
+plot(treeUPGMA, main = "UPGMA", cex = 1)
+plot(treeNJ, "unrooted", main = "NJ", cex = 1)
+
+parsimony(treeUPGMA, GST)  # 317 <-- most parsimonious tree, has lowest score
+parsimony(treeNJ, GST)     # 322
+
+optParsUPGMA = optim.parsimony(treeUPGMA, GST)
+optParsNJ = optim.parsimony(treeNJ, GST)
+
+plot(optParsUPGMA, main="UPGMA", cex = 1) # rooted tree on top
+plot(optParsNJ, "unrooted", main="NJ", cex = 1) # unrooted tree on bottom
+
+
+fit_treeUPGMA = pml(unroot(treeUPGMA), data = GST)
+
+fit_treeUPGMA_opt1 = optim.pml(fit_treeUPGMA)
+layout(matrix(c(1,2)), height=c(1,1))
+par(mar = c(.1,.1,.1,.1))
+plot(fit_treeUPGMA, main="default branches", cex = 0.8)   # top = default branch lengths
+plot(fit_treeUPGMA_opt1, main="optimized branches", cex = 0.8)   # bottom = optimized branch lengths
+
+# In the graphics window, the tree on top shows the default branches, while the tree on the bottom shows the branches after optimization. 
+# Longer branches indicate that more molecular change has occurred along a given branch. 
+# You will find that the likelihood of the data under optimized branch lengths has increased considerably - from 1573.4 to 1555. 
+# We can compare these models using the 'AIC' function and we find unsurprisingly that the AIC is substantially lower for the tree with branches optimized.
+
+AIC(fit_treeUPGMA, fit_treeUPGMA_opt1)
+
+#We can also search for a better tree by re-arranging the branches, i.e., by setting the parameter 'optNni' to 'TRUE', which causes the function 'optim.pml' to optimize tree topology in addition to branch lengths:
+
+
+fit_treeUPGMA_opt2 = optim.pml(fit_treeUPGMA, optNni=TRUE)
+
+layout(matrix(c(1,2)), height=c(1,1))
+
+plot(fit_treeUPGMA_opt1, cex = 0.8)  # top = original topology with optimized branch lengths
+plot(fit_treeUPGMA_opt2, cex = 0.8)    # bottom = optimized topology AND branch lengths
+
+AIC(fit_treeUPGMA_opt1, fit_treeUPGMA_opt2)
+
+
+##### GP60
+GP60  = read.phyDat("~/Cryptosporidium-BSc/Analysis/Trees 2.0/GP60-Tree.phy")
 dm    = dist.dna(as.DNAbin(GP60))
 
 treeUPGMA = upgma(dm)
@@ -10,8 +111,8 @@ treeNJ    = NJ(dm)
 
 layout(matrix(c(1,2)), height=c(2,2.5))
 
-plot(treeUPGMA, main = "UPGMA", cex = 0.2)
-plot(treeNJ, "unrooted", main = "NJ", cex = 0.2)
+plot(treeUPGMA, main = "UPGMA", cex = 1)
+plot(treeNJ, "unrooted", main = "NJ", cex = 1)
 
 parsimony(treeUPGMA, GP60)  # 317 <-- most parsimonious tree, has lowest score
 parsimony(treeNJ, GP60)     # 322
@@ -19,9 +120,33 @@ parsimony(treeNJ, GP60)     # 322
 optParsUPGMA = optim.parsimony(treeUPGMA, GP60)
 optParsNJ = optim.parsimony(treeNJ, GP60)
 
-plot(optParsUPGMA, main="UPGMA", cex = 0.8) # rooted tree on top
-plot(optParsNJ, "unrooted", main="NJ", cex = 0.5) # unrooted tree on bottom
+plot(optParsUPGMA, main="UPGMA", cex = 1) # rooted tree on top
+plot(optParsNJ, "unrooted", main="NJ", cex = 1) # unrooted tree on bottom
 
-#### ML
 
 fit_treeUPGMA = pml(unroot(treeUPGMA), data = GP60)
+
+fit_treeUPGMA_opt1 = optim.pml(fit_treeUPGMA)
+layout(matrix(c(1,2)), height=c(1,1))
+par(mar = c(.1,.1,.1,.1))
+plot(fit_treeUPGMA, main="default branches", cex = 0.8)   # top = default branch lengths
+plot(fit_treeUPGMA_opt1, main="optimized branches", cex = 0.8)   # bottom = optimized branch lengths
+
+# In the graphics window, the tree on top shows the default branches, while the tree on the bottom shows the branches after optimization. 
+# Longer branches indicate that more molecular change has occurred along a given branch. 
+# You will find that the likelihood of the data under optimized branch lengths has increased considerably - from 1573.4 to 1555. 
+# We can compare these models using the 'AIC' function and we find unsurprisingly that the AIC is substantially lower for the tree with branches optimized.
+
+AIC(fit_treeUPGMA, fit_treeUPGMA_opt1)
+
+#We can also search for a better tree by re-arranging the branches, i.e., by setting the parameter 'optNni' to 'TRUE', which causes the function 'optim.pml' to optimize tree topology in addition to branch lengths:
+
+
+fit_treeUPGMA_opt2 = optim.pml(fit_treeUPGMA, optNni=TRUE)
+
+layout(matrix(c(1,2)), height=c(1,1))
+
+plot(fit_treeUPGMA_opt1, cex = 0.8)  # top = original topology with optimized branch lengths
+plot(fit_treeUPGMA_opt2, cex = 0.8)    # bottom = optimized topology AND branch lengths
+
+AIC(fit_treeUPGMA_opt1, fit_treeUPGMA_opt2)
