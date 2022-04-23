26e0edbe5fae07ef97f82c76ac52dc34b874a317
Finn <tlobnow@googlemail.com>
2022-04-23 19:53:58 +0200

    updated

---
+++ Analysis/HMHZ_Phylogenetic_Analysis.R
@@ -0,0 +1,11 @@
+library(ctv)
+library(ape)
+library(ggtree)
+library(phytools)
+library(ggtree)
+library(tidyverse)
+library(tibble)
+library(cowplot)
+
+
+tree <- read.tree("https://raw.githubusercontent.com/tlobnow/Cryptosporidium-BSc/Main-Branch/Analysis/Trees%202.0/COWP-Tree.phy_phyml_tree.txt")
